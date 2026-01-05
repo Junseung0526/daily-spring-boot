58fa9d1f383679fd8f6c6c2103fcef067868d7dc
Gri22ly <79156923+Junseung0526@users.noreply.github.com>
2026-01-05 15:41:38 +0900

    Spring feat: DB 유저 조회를 위한 시큐리티 서비스 인터페이스 신규 구현

---
+++ daily-spring-boot/src/main/java/com/example/daily/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.daily.security;
+
+import com.example.daily.entity.User;
+import com.example.daily.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 1. DB에서 유저 조회
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username));
+
+        // 2. 조회한 유저를 UserDetailsImpl 상자에 담아서 반환
+        return new UserDetailsImpl(user);
+    }
+}
